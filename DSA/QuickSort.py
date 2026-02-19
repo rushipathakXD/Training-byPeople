003bd8a66fc6f68084a1ee9f2c17d62bc6bc9d1f
rushipathakXD <139609131+rushipathakXD@users.noreply.github.com>
2026-02-19 12:26:27 +0530

    Add files via upload

---
+++ DSA/QuickSort.py
@@ -0,0 +1,32 @@
+
+def partition(arr, low, high):
+    
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i, j)
+
+    swap(arr, i + 1, high)
+    return i + 1
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def quickSort(arr, low, high):
+    if low < high:
+        
+        pi = partition(arr, low, high)
+
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+
+    quickSort(arr, 0, n - 1)
+    
+    for val in arr:
+        print(val, end=" ")
